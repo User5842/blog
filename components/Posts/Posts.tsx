a4f8844dc410c523c0a4f4b825a3201e8268963b
Rafael Negron <r.negron1@umiami.edu>
2022-05-21 15:00:46 -0400

    add loading element

--- components/Posts/Posts.tsx
+++ components/Posts/Posts.tsx
@@ -1,10 +1,11 @@
-import { FunctionComponent, useContext, useEffect } from "react";
+import { FunctionComponent, useContext, useEffect, useState } from "react";
 
 import styles from "./Posts.module.css";
 
 import useIntersection from "../../hooks/useIntersection";
 import { IPost } from "../../interfaces/IPost";
 import BlogContext from "../../store/store";
+import Loader from "../Loader/Loader";
 import Post from "../Post/Post";
 
 type JSONResponse = {
@@ -14,20 +15,26 @@ type JSONResponse = {
 const Posts: FunctionComponent = () => {
   const context = useContext(BlogContext);
 
+  const [loading, setLoading] = useState(false);
+
   const [containerRef] = useIntersection(context);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`/api/posts/${context.page}`)
       .then((response) => response.json() as JSONResponse)
-      .then(({ data }) => data && context.setPosts(data));
+      .then(({ data }) => data && context.setPosts(data))
+      .finally(() => setLoading(false));
   }, [context.page]);
 
   return (
     <>
       <div className={styles.posts}>
-        {context.posts.map((post, index) => (
-          <Post key={index} {...post} />
-        ))}
+        {loading ? (
+          <Loader />
+        ) : (
+          context.posts.map((post, index) => <Post key={index} {...post} />)
+        )}
       </div>
       <hr className={styles.horizontal} ref={containerRef} />
     </>
